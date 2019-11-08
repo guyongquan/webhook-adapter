c0ea5c9cb4bb5225ae0778333fd4c6c34a586b7f
guyongquan <guyongquan@outlook.com>
2019-11-08 17:38:04 +0800

    完善钉钉

--- prometheusalert/dingtalk.js
+++ prometheusalert/dingtalk.js
@@ -1,8 +1,20 @@
 exports.template = function(body) {
+    //钉钉群机器人 webhook api 文档 https://ding-doc.dingtalk.com/doc#/serverapi2/qf2nxq
+    var alerts = body.alerts;
+    var content = alerts.map(
+        alert => {
+            return [`# Name:${alert.labels.alertname}`, "## Labels:"]
+            .concat(Object.entries(alert.labels).map(label => `- ${label[0]}:${label[1]}`))
+            .concat("## Annotations:")
+            .concat(Object.entries(alert.annotations).map(annotation => `- ${annotation[0]}:${annotation[1]}`))
+            .join("\n")
+        }
+    ).concat(`**Status:${body.status}**`).join("\n\n");
     return {
-        msgtype: "text",
-        text: {
-            content: `APE alert:${body.alerts[0].annotations.summary}`
+        msgtype: "markdown",
+        markdown: {
+            title: "ALERT",
+            text: content
         }
     }
 }
