4e9edcbba592d3333e005dfc7053510e83e5dedb
guyongquan <guyongquan@outlook.com>
2019-11-08 16:53:35 +0800

    完善微信

--- prometheusalert/wx.js
+++ prometheusalert/wx.js
@@ -1,9 +1,21 @@
 exports.template = function(body) {
+    //企业微信群机器人API，https://work.weixin.qq.com/help?person_id=1&doc_id=13376#markdown%E7%B1%BB%E5%9E%8B
+    //prometheus alert manager webhook ： https://prometheus.io/docs/alerting/configuration/#webhook_config
+    var alerts = body.alerts;
+    var content = alerts.map(
+        alert => {
+            return [`# Name:${alert.labels.alertname}`, "## Labels:"]
+            .concat(Object.entries(alert.labels).map(label => `<font color="comment">${label[0]}:</font>${label[1]}`))
+            .concat("## Annotations:")
+            .concat(Object.entries(alert.annotations).map(annotation => `<font color="comment">${annotation[0]}:</font>${annotation[1]}`))
+            .join("\n")
+        }
+    ).concat(`<font color="comment">Status:</font><font color="${body.status === 'firing' ? 'warning' : 'info'}">${body.status}</font>`).join("\n\n");
     return {
         
-        msgtype: "text",
-        text: {
-            content: body.alerts[0].annotations.summary
+        msgtype: "markdown",
+        markdown: {
+            content: content
         }
     }
 }
